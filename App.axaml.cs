e2e4dc896f16c46635144b52943ad59050a2bd0e
Lukas <lu.unruh@gmail.de>
2026-07-29 21:46:44 +0200

    Add OMDb metadata service

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Videnda.Data;
+using Videnda.Services;
 using Videnda.ViewModels;
 using Videnda.Views;
 
@@ -16,10 +19,9 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        // Legt die SQLite-Datei samt Tabellen an, falls sie noch nicht existiert
         using (var db = new VidendaContext())
             db.Database.EnsureCreated();
-
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
